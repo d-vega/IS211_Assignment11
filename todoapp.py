4ce4872970b56a9ed74b0e7688e09f69660852e1
Diandra <diandrav@DESKTOP-KDJAQVJ.localdomain>
2019-04-15 00:41:27 -0400

    Part IV nearly complete, missing REGEX

--- todoapp.py
+++ todoapp.py
@@ -5,14 +5,22 @@
 
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-to_do_tasks = [('Wash Car', 'joe.smith@summers.com', 'High'),
-               ('Buy Bread', 'jon.snow@got.com', 'High'),
-               ('Walk Dog', 'lundo@test.com', 'Medium')]
+to_do_tasks = []
+
 
 
 @app.route('/')
-def hello_world():
-    return render_template('todo.html', to_do_tasks=to_do_tasks)
+def to_do_form():
+    return render_template('index.html', to_do_tasks=to_do_tasks)
+
+
+@app.route('/submit', methods=['POST'])
+def create_to_do_list():
+    task = request.form['task']
+    email = request.form['email']
+    priority = request.form['priority']
+    to_do_tasks.append((task, email, priority))
+    return redirect('/')
 
 
 if __name__ == '__main__':
